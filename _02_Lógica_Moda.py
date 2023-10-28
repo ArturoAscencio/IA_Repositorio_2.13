dab2a3b95b5a84f97f3b20ee2d21afe7d9be20d2
ArturoAscencio <arturo.ascencio.r@gmail.com>
2023-10-27 22:48:58 -0600

    Practicas_Rep_2.13

---
+++ _02_Lógica_Moda.py
@@ -0,0 +1,22 @@
+pip install modal-logic
+
+from modal_logic import Formula, K, M
+
+# Definir fórmulas
+p = Formula('p')
+q = Formula('q')
+r = Formula('r')
+
+# Crear una fórmula modal: "Es necesario que p o q"
+necesario = K(p | q)
+
+# Imprimir la fórmula modal
+print(necesario)
+
+# Comprobar si una fórmula es válida en un mundo posible
+mundo_posible = M()
+mundo_posible.add_fact(p)
+
+# Verificar si la fórmula "Es necesario que p o q" es válida en el mundo posible
+es_valida = mundo_posible.is_valid(necesario)
+print(f"¿La fórmula es válida en el mundo posible? {es_valida}")
